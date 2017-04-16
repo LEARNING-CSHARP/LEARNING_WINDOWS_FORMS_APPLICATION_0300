ec37a0e657b9bcff82508e31bde51af2ce836bf4
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-04-16 17:45:13 +0430

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,35 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void button2_Click(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void button3_Click(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void button4_Click(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void button5_Click(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+	}
+}
